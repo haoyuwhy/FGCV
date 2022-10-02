cc2411b38c87dd03307b961a7d8e69417cfd3c08
haoyuwhy <haoyuwhy@outlook.com>
2022-10-02 15:53:33 +0800

    ema bug fix

--- models/pim_model.py
+++ models/pim_model.py
@@ -49,7 +49,8 @@ class FGVC_PIM(BaseModel):
             # load pretrained model
             load_path = self.opt['path'].get('pretrain_network_g', None)
             if load_path is not None:
-                self.load_network(self.net_g_ema, load_path, self.opt['path'].get('strict_load_g', True), 'params_ema')
+                param_key = self.opt['path'].get('param_key_g', 'params')
+                self.load_network(self.net_g_ema, load_path, self.opt['path'].get('strict_load_g', True), param_key)
             else:
                 self.model_ema(0)  # copy net_g weight
             self.net_g_ema.eval()
